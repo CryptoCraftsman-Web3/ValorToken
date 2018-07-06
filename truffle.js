2b68d452820cbb65fcadcd2ef59f3e3ed615dce3
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2018-07-06 15:25:42 +0200

    minor fix in truffle.js

--- truffle.js
+++ truffle.js
@@ -13,6 +13,25 @@
  */
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+   solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    
+    test: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*",
+    }
+  }
+
 };
