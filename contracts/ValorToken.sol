6d8cc32b62252fa40158f55bee3928f1fd2366a4
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2018-07-21 17:24:59 -0300

    made the SmartValor token burnable

--- contracts/ValorToken.sol
+++ contracts/ValorToken.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.4.24;
 
 
-import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/StandardBurnableToken.sol";
+
 
 
 /**
@@ -10,7 +11,7 @@ import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
  * Note they can later distribute these tokens as they wish using `transfer` and other
  * `StandardToken` functions.
  */
-contract ValorToken is StandardToken {
+contract ValorToken is StandardBurnableToken {
 
   string public constant name    =   "ValorToken"; //  to be approved
   string public constant symbol  =   "VALOR";      //  to be approved
