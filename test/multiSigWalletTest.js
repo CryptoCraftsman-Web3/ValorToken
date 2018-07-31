aec966521f4915bdfa669ff6187d5bb8f4102632
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2018-07-31 10:50:41 -0300

    small changed in multisig wallet

--- test/multiSigWalletTest.js
+++ test/multiSigWalletTest.js
@@ -17,25 +17,19 @@ contract('MultiSigWallet', (accounts) => {
     it('Transfers SmartValor tokens after 2 confirmations', async () => {
         // Issue tokens to the multisig address
         const issueResult = await valorToken.transfer(multisigInstance.address, 1000000, {from: accounts[0]})
-        assert.ok(issueResult)
-
-        let txCount = await multisigInstance.getTransactionCount(true, true);
 
         // Encode transfer call for the multisig
         const transferEncoded = valorToken.contract.transfer.getData(accounts[1], 1000000)
 
         let transactionId = await multisigInstance.submitTransaction(valorToken.address, 0, transferEncoded, {from: accounts[0]});
+        console.log(transactionId)
 
-         txCount = await multisigInstance.getTransactionCount(true, true);
-
-        let confirmationCount = await multisigInstance.getConfirmationCount.call(0);
-
-        assert.equal(1, confirmationCount.toNumber());
+        // todo fix this call
         const executedTransactionId = await multisigInstance.confirmTransaction(0, {from: accounts[1]});
 
 
         // Check that transaction has been executed
-        //assert.ok(transactionId.equals(executedTransactionId))
+        assert.ok(transactionId.equals(executedTransactionId))
 
           // Check that the transfer has actually occured
         assert.equal(1000000, await valorToken.balanceOf.call(accounts[1]))
