302ac3bab4bf9abf9d4c23616fb255961e2200f4
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2018-08-01 18:31:36 -0300

    generated method submitERC20Transaction and added call to MultiSigWallet contract

--- contracts/multisig/ERC20MultiSigWallet.sol
+++ contracts/multisig/ERC20MultiSigWallet.sol
@@ -6,7 +6,19 @@ import "./MultiSigWallet.sol";
  * @title ERC20MultiSigWallet
  * @dev ERC20 Token MultiSigWallet. Extends Consensys MultiSigWallet and adds a method to operate
  * @dev only with ERC20 tokens, instead of Ethers.
+*/
 
  contract ERC20MultiSigWallet is MultiSigWallet {
 
+   /**
+    * @dev adds transaction with default values for address and ethers amounts
+    * @dev and call to ERC20 transfer goes in payload of _data.
+    * @param _data payload of the command to execute.
+  */
+   function submitERC20Transaction(bytes _data)
+   public
+   returns (uint transactionId)
+   {
+      submitTransaction(address(this), 0, _data)
+   }
 }
