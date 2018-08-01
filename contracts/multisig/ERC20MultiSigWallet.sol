622797f18b5130d101c93e208bda6a145f3314f9
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2018-08-01 10:26:47 -0300

    initial structure for ERC20MultiSigContract created

---
+++ contracts/multisig/ERC20MultiSigWallet.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.24;
+
+import "./MultiSigWallet.sol";
+
+/**
+ * @title ERC20MultiSigWallet
+ * @dev ERC20 Token MultiSigWallet. Extends Consensys MultiSigWallet and adds a method to operate
+ * @dev only with ERC20 tokens, instead of Ethers.
+
+ contract ERC20MultiSigWallet is MultiSigWallet {
+
+}
