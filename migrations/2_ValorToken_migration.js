9449c061c045155944d0abdc6904a9d1c3f73f3d
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2018-08-08 12:05:59 -0300

    modified deployment for new constructor in develop network

--- migrations/2_ValorToken_migration.js
+++ migrations/2_ValorToken_migration.js
@@ -1,5 +1,8 @@
 var ValorToken = artifacts.require("./ValorToken.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(ValorToken);
+module.exports = function(deployer, network, accounts) {
+  // for now we are deploying into network with three accounts
+  if (network == "develop"){
+    deployer.deploy(ValorToken, accounts[7], accounts[8], accounts[9]);
+  }
 };
