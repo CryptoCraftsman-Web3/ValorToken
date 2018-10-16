198b6bb587bf99a2102427d916e03e08d93642ee
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2018-10-16 09:57:11 +0200

    initial_supply assert

--- contracts/ValorToken.sol
+++ contracts/ValorToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.25;
 
 import "openzeppelin-solidity/contracts/token/ERC20/StandardBurnableToken.sol";
 /**
@@ -41,6 +41,8 @@ contract ValorToken is StandardBurnableToken {
         employeePool  = _employeePool;
         futureDevFund = _futureDevFund;
         companyWallet = _companyWallet;
+
+        assert(INITIAL_SUPPLY == employeePoolSupply + futureDevFundSupply + companyWalletSupply);
 
         totalSupply_ = INITIAL_SUPPLY;
 
